4bd8235bb8c1e2570201b44a107cd1fe16e8023d
Ahmad Bilal <ahmadbilal339@gmail.com>
2023-05-08 23:12:04 +0500

    pricing section button and text code updated to fully responsive

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
 'use client';
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
 import { ChakraProvider } from '@chakra-ui/react'
 import Header from './Header';
 import Pricing from './Pricing';
